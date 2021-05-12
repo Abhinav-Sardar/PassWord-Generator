fc5450eb40cd59f16ccbd0c0375babc3e8054eda
Abhinav-Sardar <abhinavsardar.podar@gmail.com>
2021-05-12 15:33:31 +0530

    Added PassWord Generator

--- src/index.js
+++ src/index.js
@@ -3,32 +3,90 @@ import React, { useEffect, useState  } from 'react' ;
 import './index.css' ;  
 
 let App = () => {
+
+    const [checkboxes, setCheckboxes] = useState({alpha:false , number:false  , SC:false}) ; 
+    const [password , setPassword] = useState('') ; 
+    const [input , setInput] = useState('') ; 
     const Alphabets = 'abcdefghijklmnopqrstuvwxtzABCDEFGHIJKLMNOPQRSTUVWXYZ' ; 
     const numbers = '1234567890' ; 
     const specialCharacters = '!@#$%^&*()' ; 
+    const handleSubmit = (e) => {
+        e.preventDefault() ; 
+        if(isNaN(input)){
+            alert('Invalid Length!')
+        }
+        else if(!checkboxes.alpha&&!checkboxes.number&&!checkboxes.SC){
+            alert('Please Select A field')
+        }
+        else {
+            let arr = [] ; 
+            if(checkboxes.alpha){
+                arr.push('alpha')
 
+            }
+            if(checkboxes.number){
+                arr.push('number') ; 
+            }
+            if(checkboxes.SC){
+                arr.push('SC')
+            }
+            let word = '' ; 
+            while (word.length !== Number(input)) {
+                let randomField = arr[Math.floor(Math.random()*arr.length)] ; 
+                switch (randomField) {
+                    case 'alpha':
+                        word += Alphabets[Math.floor(Math.random()*Alphabets.length)] ;
+                        break ;  
+                    case 'number' :
+                        word += numbers[Math.floor(Math.random()*numbers.length)] ; 
+                        break ; 
+                    case 'SC':
+                        word += specialCharacters[Math.floor(Math.random()*specialCharacters.length)]; 
+                    default : 
 
-
+                }
+                
+            }
+            console.log(word);
+            setPassword(word) ;  
+        }
+    }
     return(
     
         <React.Fragment>
-            <form onSubmit = {(e) => {
-                e.preventDefault()
-            }}>
+            <center><h1>Password Generator</h1></center>
+            <h2>Criterias</h2>
+            <form onSubmit ={handleSubmit}>
             <div className="cont">
-        <input type="checkbox" name="" id="" /><span>Should Contain ALphabets</span><br />
+        <input type="checkbox" name="" id=""  
+        checked = {checkboxes.alpha}
+        onChange = {() => setCheckboxes({...checkboxes , alpha:!checkboxes.alpha})}
+        
+        /><span>Should Contain ALphabets</span><br />
         </div>
         <div className="cont">
-        <input type="checkbox" name="" id="" /><span>Should Contain Numbers</span> <br />
+        <input type="checkbox" name="" id="" 
+        checked = {checkboxes.number}
+        onChange = {(e) => setCheckboxes({...checkboxes , number:!checkboxes.number}) }
+        
+        /><span>Should Contain Numbers</span> <br />
         </div>
-        <div className="cont"><input type="checkbox" name="" id="" /><span>Should Contain Special Characters</span></div>
+        <div className="cont"><input type="checkbox" name="" id=""
+        checked = {checkboxes.SC}
+        onChange = {() => setCheckboxes({...checkboxes , SC:!checkboxes.SC})}
+        
+        /><span>Should Contain Special Characters</span></div>
+        <h2>What should Be the lengh of your password ?( number expected )</h2>
+        <input type="text" style = {{marginLeft:'10px'}} 
+        value = {input}
+        onChange = {(e) => setInput(e.target.value) }
+        /> <br /><br />
+        <button type="submit" className="btn" style = {{marginLeft:10}}>Submit</button>
         </form>
 
-        <h2>What should Be the lengh of your password ?</h2>
-        <input type="text" style = {{marginLeft:'10px'}} /> <br /><br />
-        <button type="submit" className="btn" style = {{marginLeft:10}}>Submit</button>
-
-        <center></center>
+        <center>
+            <div className="answer">{password}</div>
+        </center>
 
         </React.Fragment>
     )
